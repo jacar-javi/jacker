d276d5f41b6ee05b5fe532e5581079bf59e82a4d
Javier Cañete Arroyo <javi@jacar.es>
2024-03-19 18:02:54 +0100

    Minor Bugfixes

--- stacks/newstack.sh
+++ stacks/newstack.sh
@@ -2,11 +2,11 @@
 cd "$(dirname "$0")"
 source ../.env
 
-if [ "$#" -neq 1 ]; then
+if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <name-of-new-stack>"
     exit -1;
 fi
 
 cp -r ../templates/base_stack ./$1
 
-echo $(basename "$0") succesfully created $1 from Jacker's base_stack template
+echo Succesfully created $1 from base_stack template
