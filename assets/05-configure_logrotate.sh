51d4626af4c01c0829a2069fb6538678f9ef7f78
Javier Cañete Arroyo <javi@jacar.es>
2024-03-19 23:36:22 +0100

    Fixed and Tested

--- assets/05-configure_logrotate.sh
+++ assets/05-configure_logrotate.sh
@@ -2,6 +2,11 @@
 cd "$(dirname "$0")"
 source ../.env
 
-sudo envsubst < $DOCKER_DIR/assets/logrotate/traefik > /etc/logrotate.d/traefik
+export DOCKERDIR=$DOCKERDIR
+
+envsubst < logrotate/traefik.template > logrotate/traefik
+sudo mv logrotate/traefik /etc/logrotate.d/
+sudo chown root.root /etc/logrotate.d/traefik
+sudo chmod 644 /etc/logrotate.d/traefik
 
 echo Finished $(basename "$0")
