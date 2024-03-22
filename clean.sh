3152c2f5a14a5e332534a4f9095c4f63a9a60657
Javier Cañete Arroyo <javi@jacar.es>
2024-03-22 07:54:00 +0100

    Clean Script

---
+++ clean.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+LAST_PWD=`pwd`
+cd "$(dirname "$0")"
+
+read -r -p "All existing data will be removed. Do you want to continue? [y/N] " response
+case $response in
+  [yY][eE][sS]|[yY])
+    docker compose down & > /dev/null
+    docker image prune -a -f & > /dev/null
+    sudo rm -rf data/crowdsec || true
+    sudo rm -rf data/mysql || true
+    sudo rm -rf data/portainer || true
+    sudo rm -rf data/traefik/acme.json || true
+    sudo rm -rf secrets/traefik_forward_oauth || true
+    sudo rm -rf logs || true
+    sudo rm -rf .env || true
+ ;;
+  *)
+    exit -1
+  ;;
+esac
+
+cd $LAST_PWD
