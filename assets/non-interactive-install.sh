93fb801660b73e0ba51de8e6283c5fe3eb4216fd
Javier Cañete Arroyo <jcanete@theadwatch.com>
2025-10-11 21:04:45 +0000

    fix: add missing color exports

--- assets/non-interactive-install.sh
+++ assets/non-interactive-install.sh
@@ -15,6 +15,9 @@ export RED='\033[0;31m'
 export GREEN='\033[0;32m'
 export YELLOW='\033[1;33m'
 export BLUE='\033[0;34m'
+export MAGENTA='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[1;37m'
 export NC='\033[0m'
 
 # Unicode symbols
