c2db2300ec31d6b5cd9b0c7890388f653cf8a648
Javier Cañete Arroyo <jcanete@theadwatch.com>
2025-10-18 12:11:08 +0000

    fix(redis): grant oauth_user access to renamed commands (DEL, EXPIRE, TTL)
    OAuth2-proxy was failing with 'ERR unknown command del' because we renamed
    dangerous commands in redis.conf for security.
    
    Added explicit permissions for renamed commands:
    - +DEL_b6c7d8e9f1a2 (session deletion)
    - +EXPIRE_a2b3c4d5e6f7 (session expiration)
    - +PEXPIRE_f7e6d5c4b3a2 (precise expiration)
    - +TTL_d9e8f7a6b5c4 (check TTL)
    - +PTTL_c4b5a6f7e8d9 (precise TTL)
    
    Also granted ratelimit_user access to DEL, EXPIRE, TTL for rate limiting.

--- config/redis/scripts/init-acl.sh
+++ config/redis/scripts/init-acl.sh
@@ -23,11 +23,11 @@ fi
 echo "✓ Passwords loaded from secrets"
 
 # Generate ACL file from template
-# Note: OAuth user gets access to all keys temporarily for debugging
+# Note: OAuth user needs renamed commands for session management
 cat > /usr/local/etc/redis/users.acl << EOF
 user default on >${DEFAULT_PASSWORD} +@all ~* &*
-user oauth_user on >${OAUTH_PASSWORD} -@all +@read +@write +@string +@hash +@keyspace +@connection +eval +evalsha +script ~* resetchannels
-user ratelimit_user on >${RATELIMIT_PASSWORD} -@all +@read +@write +@string +@keyspace +@connection +select +eval +evalsha +script ~* resetchannels
+user oauth_user on >${OAUTH_PASSWORD} -@all +@read +@write +@string +@hash +@keyspace +@connection +eval +evalsha +script +DEL_b6c7d8e9f1a2 +EXPIRE_a2b3c4d5e6f7 +PEXPIRE_f7e6d5c4b3a2 +TTL_d9e8f7a6b5c4 +PTTL_c4b5a6f7e8d9 ~* resetchannels
+user ratelimit_user on >${RATELIMIT_PASSWORD} -@all +@read +@write +@string +@keyspace +@connection +select +eval +evalsha +script +DEL_b6c7d8e9f1a2 +EXPIRE_a2b3c4d5e6f7 +TTL_d9e8f7a6b5c4 ~* resetchannels
 user exporter_user on >${EXPORTER_PASSWORD} -@all +@read +@keyspace +@connection +info +config|get ~* resetchannels
 EOF
 
